600e1e778d99c2cfda45045e809932388150fe84
Colin Gutow <cgutow22@wooster.edu>
2020-05-01 22:31:32 -0500

    CS final project, fixed the database and enabled the website to function, features.md contains a brief overview of included features and the link to the website

--- Plants_api.py
+++ Plants_api.py
@@ -1,12 +1,13 @@
-from flask import Flask, g, jsonify, request
+from flask import Flask, g, jsonify, request, render_template
 from flask.views import MethodView
 import os
+import requests
+import sys
 from Plants_db import PlantsDatabase
 
 app = Flask(__name__)
 
 app.config['DATABASE'] = os.path.join(app.root_path, 'Plants.sqlite')
-
 
 def get_db():
     """
@@ -64,6 +65,9 @@ def handle_invalid_usage(error):
     """
     return error.to_response()
 
+@app.route('/')
+def main():
+    return render_template('Colin_2020_cs_project_page.html')
 
 class PlotsView(MethodView):
     """
@@ -81,8 +85,8 @@ class PlotsView(MethodView):
         :return: JSON response
         """
         if plot_id is None:
-            plants = get_db().get_all_plots()
-            return jsonify(plants)
+            plot = get_db().get_all_plots()
+            return jsonify(plot)
         else:
             plot = get_db().get_plot_by_id(plot_id)
 
@@ -102,12 +106,13 @@ class PlotsView(MethodView):
         :return: JSON response representing the new plot
         """
 
-        for parameter in ('sunlight', 'pH'):
+        for parameter in ('sunlight', 'pH', 'fertilizer_type'):
             if parameter not in request.form:
                 error = 'parameter {} required'.format(parameter)
                 raise RequestError(422, error)
 
-        plot = get_db().insert_plot(request.form['sunlight'], request.form['pH'])
+        plot = get_db().insert_plot(request.form['sunlight'], request.form['pH'],
+                                    request.form['fertilizer_type'])
         return jsonify(plot)
 
     def delete(self, plot_id):
@@ -117,7 +122,7 @@ class PlotsView(MethodView):
         :param plot_id: id of a plot
         :return: JSON response containing a message
         """
-        if get_db().get_plot_by_id(plot_id) is None:
+        if get_db().get_plot_by_id() is None:
             raise RequestError(404, 'plot not found')
 
         get_db().delete_plot(plot_id)
@@ -125,125 +130,33 @@ class PlotsView(MethodView):
         return jsonify({'message': 'plot deleted successfully'})
 
 
-class PlantsView(MethodView):
+class fertilizerView(MethodView):
     """
-    This view handles all the /plants requests.
+    This view handles all the /fertilizer requests.
     """
 
-    def get(self, plant_id):
+    def get(self, fertilizer_id):
         """
         Handle GET requests.
 
         Returns JSON representing all of the plants if plant_id is None, or a
         single plant if plant_id is not None.
 
-        :param plant_id: id of a dplantog, or None for all plants
+        :param fertilizer_id: id of a fertilizer, or None for all fertilizers
         :return: JSON response
         """
-        if plant_id is None:
-            plants = get_db().get_all_plants()
-            return jsonify(plants)
+        if fertilizer_id is None:
+            fertilizer_id = get_db().get_all_fertilizer()
+            return jsonify(fertilizer_id)
         else:
-            plant = get_db().get_plant_by_id(plant_id)
+            fertilizer_id = get_db().get_plant_by_id(fertilizer_id)
 
-            if plant is not None:
-                response = jsonify(plant)
+            if fertilizer_id is not None:
+                response = jsonify(fertilizer_id)
             else:
-                raise RequestError(404, 'plant not found')
+                raise RequestError(404, 'fertilizer not found')
 
             return response
-
-    def post(self):
-        """
-        Implements POST /plants
-
-        Requires the form parameters 'plant', 'pH', and 'sunlight'
-
-        :return: JSON response representing the new dog
-        """
-
-        for parameter in ('plant', 'pH', 'sunlight'):
-            if parameter not in request.form:
-                error = 'parameter {} required'.format(parameter)
-                raise RequestError(422, error)
-
-        plant = get_db().insert_plant(request.form['plant'], request.form['pH'],
-                                  request.form['sunlight'])
-        return jsonify(plant)
-
-    def delete(self, plant_id):
-        """
-        Handle DELETE requests. The plant_id must be provided.
-
-        :param plant_id: id of a dog
-        :return: JSON response containing a message
-        """
-        if get_db().get_plant_by_id(plant_id) is None:
-            raise RequestError(404, 'plant not found')
-
-        get_db().delete_plant(plant_id)
-
-        return jsonify({'message': 'plant deleted successfully'})
-
-    class PlantsView(MethodView):
-        """
-        This view handles all the /plants requests.
-        """
-
-        def get(self, plant_id):
-            """
-            Handle GET requests.
-
-            Returns JSON representing all of the plants if plant_id is None, or a
-            single plant if plant_id is not None.
-
-            :param plant_id: id of a dplantog, or None for all plants
-            :return: JSON response
-            """
-            if plant_id is None:
-                plants = get_db().get_all_plants()
-                return jsonify(plants)
-            else:
-                plant = get_db().get_plant_by_id(plant_id)
-
-                if plant is not None:
-                    response = jsonify(plant)
-                else:
-                    raise RequestError(404, 'plant not found')
-
-                return response
-
-        def post(self):
-            """
-            Implements POST /plants
-
-            Requires the form parameters 'plant', 'pH', and 'sunlight'
-
-            :return: JSON response representing the new dog
-            """
-
-            for parameter in ('plant', 'pH', 'sunlight'):
-                if parameter not in request.form:
-                    error = 'parameter {} required'.format(parameter)
-                    raise RequestError(422, error)
-
-            plant = get_db().insert_plant(request.form['plant'], request.form['pH'],
-                                          request.form['sunlight'])
-            return jsonify(plant)
-
-        def delete(self, plant_id):
-            """
-            Handle DELETE requests. The plant_id must be provided.
-
-            :param plant_id: id of a dog
-            :return: JSON response containing a message
-            """
-            if get_db().get_plant_by_id(plant_id) is None:
-                raise RequestError(404, 'plant not found')
-
-            get_db().delete_plant(plant_id)
-
-            return jsonify({'message': 'plant deleted successfully'})
 
 
 class PlantsView(MethodView):
@@ -283,13 +196,13 @@ class PlantsView(MethodView):
         :return: JSON response representing the new dog
         """
 
-        for parameter in ('plant', 'pH', 'sunlight'):
+        for parameter in ('plant', 'pH', 'sunlight', 'fertilizer_type'):
             if parameter not in request.form:
                 error = 'parameter {} required'.format(parameter)
                 raise RequestError(422, error)
 
         plant = get_db().insert_plant(request.form['plant'], request.form['pH'],
-                                      request.form['sunlight'])
+                        request.form['sunlight'], request.form['fertilizer_type'])
         return jsonify(plant)
 
     def delete(self, plant_id):
@@ -305,3 +218,26 @@ class PlantsView(MethodView):
         get_db().delete_plant(plant_id)
 
         return jsonify({'message': 'plant deleted successfully'})
+
+#Register PlantsView as the handler for all the /plants requests
+plants_view = PlantsView.as_view('plants_view')
+app.add_url_rule('/plants', defaults={'plant_id': None},
+                 view_func=plants_view, methods=['GET', 'DELETE'])
+app.add_url_rule('/plants', view_func=plants_view, methods=['POST'])
+app.add_url_rule('/plants/<int:Plant_id>', view_func=plants_view, methods=['GET', 'DELETE'])
+
+#Register PlotsView as the handler for all the /plots requests
+plots_view = PlotsView.as_view('plots_view')
+app.add_url_rule('/plots', defaults={'plot_id': None},
+                 view_func=plots_view, methods=['GET', 'DELETE'])
+app.add_url_rule('/plots', view_func=plots_view, methods=['POST'])
+app.add_url_rule('/plots/<int:plot_id>', view_func=plots_view, methods=['GET', 'DELETE'])
+
+#Register fertilizerView as the handler for all the /fertilizer requests
+fertilizer_view = fertilizerView.as_view('fertilizer_view')
+app.add_url_rule('/fertilizer', defaults={'fertilizer_id': None},
+                 view_func=fertilizer_view, methods=['GET'])
+app.add_url_rule('/fertilizer/<int:fertilizer_id>', view_func=fertilizer_view, methods=['GET'])
+
+if __name__ == '__main__':
+        app.run(debug=True)
--- Plants_db.py
+++ Plants_db.py
@@ -1,6 +1,34 @@
+from flask import Flask, g, render_template
 import os
 import sqlite3
+from collections import OrderedDict
 
+app = Flask(__name__)
+
+app.config['DATABASE'] = os.path.join(app.root_path, 'Plants.sqlite')
+
+def connect_db():
+    """
+    Returns a sqlite connection object associated with the application's
+    database file.
+    """
+
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+
+    return conn
+
+
+def get_db():
+    """
+    Returns a database connection. If a connection has already been created,
+    the existing connection is used, otherwise it creates a new connection.
+    """
+
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+
+    return g.sqlite_db
 
 def row_to_dict_or_none(cur):
     """
@@ -47,16 +75,18 @@ class PlantsDatabase():
     def create_tables(self):
 
         cur = self.conn.cursor()
-        cur.execute('CREATE TABLE Plant(Plant_id INTEGER PRIMARY KEY, '
-                    'name TEXT UNIQUE, '
-                    'FOREIGN KEY pH REFERENCES pH (pH)'
-                    'FOREIGN KEY sunlight REFERENCES plots (sunlight)')
-        cur.execute('CREATE TABLE pH(pH INTEGER')
+        cur.execute('CREATE TABLE fertilizer(fertilizer_id INTEGER PRIMARY KEY, '
+                    'fertilizer_type TEXT UNIQUE)')
         cur.execute('CREATE TABLE plots(plot_id INTEGER PRIMARY KEY, '
-                    'sunlight TEXT, '
-                    'FOREIGN KEY pH references pH (pH)')
+                     'sunlight TEXT UNIQUE, pH INTEGER, fertilizer_type TEXT, '
+                     'FOREIGN KEY (fertilizer_type) REFERENCES fertilizer(fertilizer_type))')
+        cur.execute('CREATE TABLE Plant(Plant_id INTEGER PRIMARY KEY, '
+                     '   name TEXT UNIQUE, pH INTEGER, fertilizer_type TEXT, sunlight TEXT,'
+                     '   FOREIGN KEY (fertilizer_type) REFERENCES fertilizer(fertilizer_type), '
+                     '   FOREIGN KEY (sunlight) REFERENCES plots(sunlight))')
+        self.conn.commit()
 
-    def insert_plant(self, name, pH, sunlight,):
+    def insert_plant(self, name, pH, fertilizer_type, sunlight,):
         """
         Inserts a plant into the database.
 
@@ -64,43 +94,66 @@ class PlantsDatabase():
 
         :param name: name of the plant
         :param pH: desired soil pH
+        :param fertilizer_type: desired nutrient
         :param sunlight: amount of sunlight needed
         :return: a dict representing the plant
         """
         cur = self.conn.cursor()
-        self.insert_pH(pH)
-        self.insert_plot(sunlight, pH)
-        plot_dict = self.get_plot_by_sunlight(sunlight)
-        plot_id = plot_dict['plot_id']
-        pH_dict = self.get_pH(pH)
+        # self.insert_plot(sunlight, pH, fertilizer_type)
+        # plot_dict = self.get_plots_by_sunlight(sunlight)
+        # sunlight = plot_dict['sunlight']
 
-        query = ('INSERT INTO plant(name, pH, sunlight) '
-                 'VALUES(?, ?, ?)')
+        query = ('INSERT OR IGNORE INTO plant(name, pH, fertilizer_type, sunlight) '
+                 'VALUES(?, ?, ?, ?)')
 
-        cur.execute(query, (name, pH, sunlight))
+        cur.execute(query, (name, pH, fertilizer_type, sunlight))
         self.conn.commit()
 
         return self.get_plant_by_id(cur.lastrowid)
 
-    def get_plant_by_id(self, plant_id):
+    def get_plant_by_id(self, Plant_id):
         """
         Given a plant's primary key, return a dictionary representation of the
-        plant, or None if there is no plannt with that primary key.
+        plant, or None if there is no plant with that primary key.
 
         :param plant_id: the primary key of the plant
         :return: a dict representing the plant
         """
         cur = self.conn.cursor()
 
-        query = ('SELECT Plant.plant_id as plant_id, Plant.name as name, '
-                 'Plant.pH as pH, Plant.sunlight as sunlight, '
-                 'plots.plot_id as plot_id, '
-                 'FROM Plant, plots '
-                 'WHERE Plant.pH = plots.pH '
-                 'AND Plant.sunlight = plots.sunlight'
-                 'AND Plant.plant_id = ?')
+        query = ('SELECT Plant_id FROM Plant WHERE Plant.Plant_id = ?')
 
-        cur.execute(query, (plant_id,))
+        cur.execute(query, (Plant_id,))
+        return row_to_dict_or_none(cur)
+
+    def get_plant_by_name(self, name):
+        """
+        Given a plant's name, return a dictionary representation of the
+        plant, or None if there is no plant with that name.
+
+        :param name: the name of the plant
+        :return: a dict representing the plant
+        """
+        cur = self.conn.cursor()
+
+        query = ('SELECT Plant_id, name FROM Plant WHERE Plant.name = ?')
+
+        cur.execute(query, (name,))
+        return row_to_dict_or_none(cur)
+
+    def get_plant_by_fertilzer(self, fertilizer_type):
+        """
+        Given a fertilizer, return a dictionary representation of the
+        plants, or None if there is no plant with that nutrient.
+
+        :param fertilizer_type: the nutrient the plant needs
+        :return: a dict representing the plant
+        """
+        cur = self.conn.cursor()
+
+        query = ('SELECT Plant.fertilizer_type FROM Plant WHERE Plant.fertilizer_type = ?')
+
+        cur.execute(query, (fertilizer_type,))
         return row_to_dict_or_none(cur)
 
     def get_all_plants(self):
@@ -113,11 +166,7 @@ class PlantsDatabase():
 
         cur = self.conn.cursor()
 
-        query = ('SELECT Plant.plant_id as plant_id, Plant.name as name, '
-                 'Plant.pH as pH, Plant.sunlight as sunlight, '
-                 'plots.plot_id as plot_id '
-                 'FROM Plant, pH '
-                 'WHERE Plant.plant_id = pH.plant_id')
+        query = ('SELECT * FROM Plant')
 
         plants = []
         cur.execute(query)
@@ -125,22 +174,26 @@ class PlantsDatabase():
         for row in cur.fetchall():
             plants.append(dict(row))
 
+        print(plants)
         return plants
 
-    def insert_plot(self, sunlight, pH):
+    def insert_plot(self, sunlight, pH, fertilizer_type):
         """
         Insert a plot into the database if it does not exist.
         Return a dict representation of the plot.
 
         :param sunlight: amount of sun the plot receives
+        :param fertilizer_type: fertilizer used on the plot
         :param pH: pH of the soil in the plot
         :return: dict representing the plot
         """
         cur = self.conn.cursor()
-        query = 'INSERT INTO plots(sunlight, pH) VALUES(?)'
-        cur.execute(query, (sunlight, pH,))
+        self.insert_fertilizer(fertilizer_type)
+        query = ('INSERT OR IGNORE INTO plots(sunlight, pH, fertilizer_type) '
+                ' VALUES(?, ?, ?)')
+        cur.execute(query, (sunlight, pH, fertilizer_type))
         self.conn.commit()
-        return self.get_plots_by_name(sunlight, pH)
+        return self.get_plots_by_sunlight(sunlight)
 
     def get_all_plots(self):
         """
@@ -159,9 +212,10 @@ class PlantsDatabase():
         for row in cur.fetchall():
             plots.append(dict(row))
 
+        print (plots)
         return plots
 
-    def get_plots_by_id(self, plot_id):
+    def get_plot_by_id(self, plot_id):
         """
         Get a dictionary representation of the plot with the given primary
         key. Return None if the plot does not exist.
@@ -170,7 +224,7 @@ class PlantsDatabase():
         :return: a dict representing the plot, or None
         """
         cur = self.conn.cursor()
-        query = 'SELECT plot_id, plots FROM plots WHERE plot_id = ?'
+        query = 'SELECT plot_id FROM plots WHERE plot_id = ?'
         cur.execute(query, (plot_id,))
         return row_to_dict_or_none(cur)
 
@@ -183,7 +237,7 @@ class PlantsDatabase():
         :return: a dict representing the plot, or None
         """
         cur = self.conn.cursor()
-        query = 'SELECT plot_id, plots FROM plots WHERE sunlight = ?'
+        query = 'SELECT plot_id, sunlight FROM plots WHERE sunlight = ?'
         cur.execute(query, (sunlight,))
         return row_to_dict_or_none(cur)
 
@@ -196,11 +250,85 @@ class PlantsDatabase():
         :return: a dict representing the plot, or None
         """
         cur = self.conn.cursor()
-        query = 'SELECT plot_id, plots FROM plots WHERE pH = ?'
+        query = 'SELECT plot_id, pH FROM plots WHERE pH = ?'
         cur.execute(query, (pH,))
         return row_to_dict_or_none(cur)
 
-    def delete_plant(self, plant_id):
+    def get_plots_by_fertilizer(self, fertilizer_type):
+        """
+        Get a dictionary representation of the plots with the given fertilzer.
+        Return None if there is no such fertilizer.
+
+        :param fertilizer_type: fertilizer used on the plot
+        :return: a dict representing the plot, or None
+        """
+        cur = self.conn.cursor()
+        query = 'SELECT plot_id, fertilizer_type FROM plots WHERE fertilizer_type = ?'
+        cur.execute(query, (fertilizer_type,))
+        return row_to_dict_or_none(cur)
+
+    def insert_fertilizer(self, fertilizer_type):
+        """
+        Inserts a fertilizer into the database.
+
+        Returns a dictionary representation of the plant.
+        :param fertilizer_typetype: which nutrient it contains (nitrogen, phosphorus, potassium)
+        :return: a dict representing the fertilizer
+        """
+        cur = self.conn.cursor()
+        query = 'INSERT OR IGNORE INTO fertilizer(fertilizer_type) VALUES(?)'
+        cur.execute(query, (fertilizer_type,))
+        self.conn.commit()
+        return self.get_fertilizer_by_id(cur.lastrowid)
+
+    def get_all_fertilizer(self):
+        """
+               Get a list of dictionary representations of all the plots in the
+               database.
+
+               :return: list of dicts representing all plots
+               """
+        cur = self.conn.cursor()
+
+        query = 'SELECT * FROM fertilizer'
+
+        fertilizer = []
+        cur.execute(query)
+
+        for row in cur.fetchall():
+            fertilizer.append(dict(row))
+
+        print(fertilizer)
+        return fertilizer
+
+
+    def get_fertilizer_by_id(self, fertilizer_id):
+        """
+        Get a dictionary representation of the fertilizer with the given primary
+        key. Return None if the fertilizer does not exist.
+
+        :param fertilizer_id: primary key of the fertilizer
+        :return: a dict representing the fertilizer, or None
+        """
+        cur = self.conn.cursor()
+        query = 'SELECT fertilizer_id, fertilizer_type FROM fertilizer WHERE Fertilizer_id = ?'
+        cur.execute(query, (fertilizer_id,))
+        return row_to_dict_or_none(cur)
+
+    def get_fertilizer_by_type(self, fertilizer_type):
+        """
+        Get a dictionary representation of the fertilizer type.
+        Return None if the plot does not exist.
+
+        :param fertilizer_type: type of fertilizer
+        :return: a dict representing the fertilizer, or None
+        """
+        cur = self.conn.cursor()
+        query = 'SELECT fertilizer_type, fertilizer_id FROM fertilizer WHERE Fertilizer_type = ?'
+        cur.execute(query, (fertilizer_type,))
+        return row_to_dict_or_none(cur)
+
+    def delete_plant(self, Plant_id):
         """
         Delete the plant with the given primary key.
 
@@ -209,9 +337,8 @@ class PlantsDatabase():
 
         cur = self.conn.cursor()
 
-        query = 'DELETE FROM Plant WHERE plant_id = ?,' \
-                'DELETE FROM pH WHERE plant_id = ?'
-        cur.execute(query, (plant_id,))
+        query = 'DELETE FROM Plant WHERE Plant_id = ?,'
+        cur.execute(query, (Plant_id,))
 
         self.conn.commit()
 
@@ -224,6 +351,24 @@ class PlantsDatabase():
 
         self.conn.commit()
 
+    def delete_fertilizer(self, fertilizer_id):
+
+        cur = self.conn.cursor()
+
+        query = 'DELETE FROM fertilizer WHERE fertilizer_id = ?,'
+        cur.execute(query, (fertilizer_id,))
+
+        self.conn.commit()
+
+    # def delete_pH(self, pH_id):
+    #
+    #     cur = self.conn.cursor()
+    #
+    #     query = 'DELETE FROM pH WHERE pH_id = ?,'
+    #     cur.execute(query, (pH_id))
+    #
+    #     self.conn.commit()
+
 
 if __name__ == '__main__':
     # Here are some rudimentary tests to make sure that it is possible to
@@ -233,24 +378,32 @@ if __name__ == '__main__':
     # explicitly run this file, and will not be run when the Flask app runs.
     # This is because when the code from this file is imported, __name__ will
     # not be '__main__', but when it is run explicitly it will be '__main__'.
-
     db = PlantsDatabase('Plants.sqlite')
-    db.insert_plot('full, 7')
-    db.insert_plot('shade, 8')
-    db.insert_plot('partial, 7')
-    db.insert_plot('full, 6')
+    db.insert_fertilizer('potassium')
+    db.insert_fertilizer('nitrogen')
+    db.insert_fertilizer('phosphate')
 
-    db.insert_plant('Roses, 7, full')
-    db.insert_plant('')
+    db.insert_plot('full', '7', 'potassium')
+    db.insert_plot('shade', '8', 'nitrogen')
+    db.insert_plot('partial', '7', 'phosphate')
+    db.insert_plot('full', '6', 'nitrogen')
+
+    db.insert_plant('Roses', '7', 'potassium', 'full')
+    db.insert_plant('Bleeding Hearts', '6', 'phosphate', 'shade')
+    db.insert_plant('Peony', '8', 'nitrogen', 'partial')
+
+    plants = db.get_all_plants()
+    print('List of all plants:', plants)
+
 
     plots = db.get_all_plots()
     print('List of all plots:', plots)
 
-    plot = db.get_plots_by_id(1)
+    plot = db.get_plot_by_id(1)
     print('plot with ID 1:', plot)
 
     plot = db.get_plots_by_pH(7)
     print('plots with pH 7:', plot)
 
     plot = db.get_plots_by_sunlight('full')
-    print('plots with full sunlight')
+    print('plots with full sunlight', plot)
